6f025a1391d555aa6248daf5bfd985dda6420444
Beletskiy <Alexey1Beletskiy@gmail.com>
2016-04-06 14:56:51 +0300

    simple todo list finish

--- app/todo_app.ts
+++ app/todo_app.ts
@@ -4,12 +4,7 @@ import {TodoList} from './todo_list';
 import {TodoForm} from './todo_form';
 @Component({
     selector: 'todo-app',
-    template: `
-    <h2>Todo</h2>
-    <span>{{remaining}} of {{todos.length}} remaining</span>
-    [ <a (click)="archive()">archive</a> ]
-    <todo-list [todos]="todos"></todo-list>
-    <todo-form (newTask)="addTask($event)"></todo-form>`,
+    templateUrl: 'app/todo_app.html',
     styles:['a { cursor: pointer; cursor: hand; }'],
     directives: [TodoList, TodoForm]
 })
---
+++ app/todo_list.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from 'angular2/core';
+import {Todo} from './todo';
+@Component({
+    selector: 'todo-list',
+    styles: [`
+    .done-true {
+      text-decoration: line-through;
+      color: grey;
+    }`
+    ],
+    templateUrl: 'app/todo_list.html',
+})
+export class TodoList {
+    @Input() todos: Todo[];
+}
